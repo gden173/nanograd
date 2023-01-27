3be60bc3f32162c3dc405f701335e1241dbb8513
gabriel <gabrieldennis17@gmail.com>
2023-01-27 19:16:53 +1000

    feat: adding graph drawings

--- nanograd/nanograd.py
+++ nanograd/nanograd.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+from typing import Tuple, Set
 import math
+import graphviz
 
 """
 An implementation of a Nano Autograd Library 
@@ -14,22 +16,25 @@ class Val:
     Scalar value class to do Autograd things on
     """
 
-    def __init__(self, data: float, label: str = "", _children: tuple = ()) -> None:
+    def __init__(
+        self, data: float, _label: str = None, _children: tuple = (), grad=True
+    ) -> None:
         self.data: float = data  # Value
-        self.label: str = label  # Value from operation
-        self.grad: float = 0  # Initial gradient
+        self._label: str = _label  # Value from operation
+        self.grad: float = 0 if grad else 0  # Initial gradient
         self._children: set[Val] = set(_children)
         self._backward = lambda: None
 
     def __add__(self, y):
         """Addition"""
         y = y if isinstance(y, Val) else Val(data=y)
-        x = Val(self.data + y.data, _children=(self, y), label="+")
+        x = Val(self.data + y.data, _children=(self, y), _label="+")
 
         # Compute the accumulations of the gradient
         def _backward():
             self.grad += x.grad
             y.grad += x.grad
+
         x._backward = _backward
         return x
 
@@ -39,11 +44,12 @@ class Val:
 
     def __mul__(self, y):
         y = y if isinstance(y, Val) else Val(data=y)
-        x = Val(self.data * y.data, _children=(self, y), label="*")
+        x = Val(self.data * y.data, _children=(self, y), _label="*")
 
         def _backward():
             self.grad += y.grad * x.grad
             y.grad += self.grad * x.grad
+
         x._backward = _backward
 
         return x
@@ -54,11 +60,13 @@ class Val:
     def __pow__(self, y):
         assert isinstance(y, (float, int)), f"{y=} must be an int or a float"
         y = y if isinstance(y, Val) else Val(data=y)
-        x = Val(self.data**y.data, label=f"**{y}", _children=(self,))
-        def _backward(): 
-            self.grad += y * self ** (y - 1) 
+        x = Val(self.data**y.data, _label=f"**{y}", _children=(self,))
+
+        def _backward():
+            self.grad += y * self ** (y - 1)
+
         x._backward = _backward
-        return x 
+        return x
 
     def __neg__(self):
         """Negation"""
@@ -86,51 +94,124 @@ class Val:
 
     def relu(self):
         """Rectified Linear Unit Activation Function"""
-        x = Val(max([0, self.data]), _children=(self,), label="ReLU")
+        x = Val(max([0, self.data]), _children=(self,), _label="ReLU")
 
-        def _backward(): 
+        def _backward():
             self.grad += (x.grad > 0) * x.grad
 
         x._backward = _backward
-        return x 
+        return x
 
-    def exp(self): 
+    def exp(self):
         """Exponential"""
-        x = Val(math.exp(self.data), _children=(self,), label="Exp")
-        def _backward(): 
+        x = Val(math.exp(self.data), _children=(self,), _label="Exp")
+
+        def _backward():
             self.grad += math.exp(self.data)
+
         x._backward = _backward
         return x
 
     def sigmoid(self):
         """Sigmoid activation Function"""
-        x = Val(self.exp()  / (self.exp() + 1), _children=(self,), label = "Sigmoid")
-        def _backward(): 
+        x = Val(self.exp() / (self.exp() + 1), _children=(self,), _label="Sigmoid")
+
+        def _backward():
             self.grad += (-self).exp() / ((-self).exp() + 1) ** 2
+
         x._backward = _backward
-        return x 
+        return x
 
     def tanh(self):
         """Tanh Activation Function"""
-        x = Val(((2 * self).exp() - 1) / ((2 * self).exp() + 1), 
-                _children=(self,),
-                label="Tanh"
+        x = Val(
+            ((2 * self).exp() - 1) / ((2 * self).exp() + 1),
+            _children=(self,),
+            _label="Tanh",
         )
-        def _backward(): 
-            self.grad += (2 / ((2 * self).exp() + 1) ** 2)
+
+        def _backward():
+            self.grad += 2 / ((2 * self).exp() + 1) ** 2
+
         x._backward = _backward
-        return x 
+        return x
 
-
-    def _build_topological_graph(self):
+    def _build_topological_graph(self, _topo: list, _visited: set) -> Tuple[list, set]:
         """Builds a Topological Graph to compute the backwards pass"""
+
+        # Check if this node has been processed
+        if self not in _visited:
+
+            # Add the current node to the list
+            _visited.add(self)
+
+            # Add all child nodes to the list
+            for child_node in self._children:
+                # Traverse the child nodes
+                t, v = child_node._build_topological_graph(_topo, _visited)
+
+                _topo.extend(t)
+                _visited.update(v)
+
+            _topo.append(self)
+
+        return _topo, _visited
+
+    def backward(self) -> None:
+        """Computes the backward pass"""
+        topo, _ = self._build_topological_graph([], set())
+        self.grad = 1.0
+        for n in reversed(topo):
+            n._backward()
+
+    def _trace_graph(self) -> Tuple[Set, Set]:
+        """Traces the graphs edges from passed in node
+        assuming that it is the root node
+        """
+        return self._process_node()
+
+    def _process_node(
+        self, _nodes: Set = set(), _edges: Set = set()
+    ) -> Tuple[set, set]:
+        if self not in _nodes:
+            _nodes.add(self)
+            for child_node in self._children:
+                _edges.add((child_node, self))
+                _nodes, _edges = Val._process_node(child_node, _nodes, _edges)
+        return _nodes, _edges
+
+    @property
+    def _sID(self):
+        """Creates a string ID for graphing"""
+        return str(id(self))
+
+    def create_graph(self, format: str = "svg", rank: str = "LR") -> graphviz.Digraph:
+        """Creates a digraph of the computational graph"""
+        nodes, edges = self._process_node()
+        digraph = graphviz.Digraph(format=format, graph_attr={"rankdir": rank})
+        for node in nodes:
+            digraph.node(
+                name=node._sID,
+                label=f"data: {node.data}: grad: {node.grad}",
+                shape="record",
+            )
+            if node._label is not None:
+                digraph.node(name=node._sID + node._label, label=node._label)
+                digraph.edge(node._sID + node._label, node._sID)
+
+        for nodeOne, nodeTwo in edges:
+            digraph.edge(nodeOne._sID, nodeTwo._sID)
+        return digraph
 
 
 if __name__ == "__main__":
-    a = Val(-4.0)
-    b = Val(2.0)
-    c = a + b
-    d = a * b
-    print(d)
-    d._backward()
-    print(a)
+    x = Val(1.0)
+    b = Val(3.0)
+    c = (x + b)
+    y = c.tanh()
+    y.backward()
+    g = y.create_graph()
+    g.render("test")
+
+
+
