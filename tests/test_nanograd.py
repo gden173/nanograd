0b41f7ab4439943e48143f0c087b6acd7deeef65
gabriel <gabrieldennis17@gmail.com>
2023-01-28 11:45:54 +1000

    tests: fixing tests layout

--- tests/test_nanograd.py
+++ tests/test_nanograd.py
@@ -1,221 +1,229 @@
-import torch
-import pytest
-from nanograd.nanograd import Val
-
 """Copies the tests from micrograd 
-   to ensure consistency"""
+   to ensure consistenanograd_c_valy"""
+
+import torch
+from nanograd.nanograd import Val
 
 TOLERANCE = 1e-6
 
 
 def test_addition():
-    na = Val(-4.0)
-    nb = Val(8.0)
-    nc = na + nb
-    nc.backward()
-    at = torch.tensor(-4.0, requires_grad=True)
-    bt = torch.tensor(8.0, requires_grad=True)
-    ct = at + bt
-    ct.retain_grad()
-    ct.backward()
-    assert nc.item() == ct.item()
-    assert at.grad.item() == na.grad
-    assert bt.grad.item() == nb.grad
-    assert ct.grad.item() == nc.grad
+    """Test addition"""
+    nanograd_a_val = Val(-4.0)
+    nanograd_b_val = Val(8.0)
+    nanograd_c_val = nanograd_a_val + nanograd_b_val
+    nanograd_c_val.backward()
+    torch_a_val = torch.tensor(-4.0, requires_grad=True)
+    torch_b_val = torch.tensor(8.0, requires_grad=True)
+    torch_c_val = torch_a_val + torch_b_val
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
+    assert nanograd_c_val.item() == torch_c_val.item()
+    assert torch_a_val.grad.item() == nanograd_a_val.grad
+    assert torch_b_val.grad.item() == nanograd_b_val.grad
+    assert torch_c_val.grad.item() == nanograd_c_val.grad
 
 
 def test_multiplication():
-    na = Val(-4.0)
-    nb = Val(8.0)
-    nc = na * nb
-    nc.backward()
-    at = torch.tensor(-4.0, requires_grad=True)
-    bt = torch.tensor(8.0, requires_grad=True)
-    ct = at * bt
-    ct.retain_grad()
-    ct.backward()
+    """Test multiplication"""
+    nanograd_a_val = Val(-4.0)
+    nanograd_b_val = Val(8.0)
+    nanograd_c_val = nanograd_a_val * nanograd_b_val
+    nanograd_c_val.backward()
+    torch_a_val = torch.tensor(-4.0, requires_grad=True)
+    torch_b_val = torch.tensor(8.0, requires_grad=True)
+    torch_c_val = torch_a_val * torch_b_val
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
 
-    assert nc.item() == ct.item()
-    assert at.grad.item() == na.grad
-    assert bt.grad.item() == nb.grad
-    assert ct.grad.item() == nc.grad
+    assert nanograd_c_val.item() == torch_c_val.item()
+    assert torch_a_val.grad.item() == nanograd_a_val.grad
+    assert torch_b_val.grad.item() == nanograd_b_val.grad
+    assert torch_c_val.grad.item() == nanograd_c_val.grad
 
 
 def test_division():
-    na = Val(-4.0)
-    nb = Val(8.0)
-    nc = na / nb
-    nc.backward()
-    at = torch.tensor(-4.0, requires_grad=True)
-    bt = torch.tensor(8.0, requires_grad=True)
-    ct = at / bt
-    ct.retain_grad()
-    ct.backward()
+    """Test division"""
+    nanograd_a_val = Val(-4.0)
+    nanograd_b_val = Val(8.0)
+    nanograd_c_val = nanograd_a_val / nanograd_b_val
+    nanograd_c_val.backward()
+    torch_a_val = torch.tensor(-4.0, requires_grad=True)
+    torch_b_val = torch.tensor(8.0, requires_grad=True)
+    torch_c_val = torch_a_val / torch_b_val
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
 
-    assert abs(nc.item() - ct.item()) < TOLERANCE
-    assert abs(at.grad.item() - na.grad) < TOLERANCE
-    assert abs(bt.grad.item() - nb.grad) < TOLERANCE
-    assert abs(ct.grad.item() - nc.grad) < TOLERANCE
+    assert abs(nanograd_c_val.item() - torch_c_val.item()) < TOLERANCE
+    assert abs(torch_a_val.grad.item() - nanograd_a_val.grad) < TOLERANCE
+    assert abs(torch_b_val.grad.item() - nanograd_b_val.grad) < TOLERANCE
+    assert abs(torch_c_val.grad.item() - nanograd_c_val.grad) < TOLERANCE
 
 
 def test_powers():
-    na = Val(4.0)
-    nc = na**4.5
-    nc.backward()
-    at = torch.tensor(4.0, requires_grad=True)
-    ct = at**4.5
-    ct.retain_grad()
-    ct.backward()
+    """Test powers"""
+    nanograd_a_val = Val(4.0)
+    nanograd_c_val = nanograd_a_val**4.5
+    nanograd_c_val.backward()
+    torch_a_val = torch.tensor(4.0, requires_grad=True)
+    torch_c_val = torch_a_val**4.5
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
 
-    assert abs(nc.item() - ct.item()) < TOLERANCE
-    assert abs(at.grad.item() - na.grad) < TOLERANCE
-    assert abs(ct.grad.item() - nc.grad) < TOLERANCE
+    assert abs(nanograd_c_val.item() - torch_c_val.item()) < TOLERANCE
+    assert abs(torch_a_val.grad.item() - nanograd_a_val.grad) < TOLERANCE
+    assert abs(torch_c_val.grad.item() - nanograd_c_val.grad) < TOLERANCE
 
 
 def test_exp():
-    na = Val(2.0)
-    nc = na.exp()
-    nc.backward()
-    at = torch.tensor(2.0, requires_grad=True)
-    ct = at.exp()
-    ct.retain_grad()
-    ct.backward()
+    """Test Exp"""
+    nanograd_a_val = Val(2.0)
+    nanograd_c_val = nanograd_a_val.exp()
+    nanograd_c_val.backward()
+    torch_a_val = torch.tensor(2.0, requires_grad=True)
+    torch_c_val = torch_a_val.exp()
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
 
-    assert abs(nc.item() - ct.item()) < TOLERANCE
-    assert abs(at.grad.item() - na.grad) < TOLERANCE
-    assert abs(ct.grad.item() - nc.grad) < TOLERANCE
+    assert abs(nanograd_c_val.item() - torch_c_val.item()) < TOLERANCE
+    assert abs(torch_a_val.grad.item() - nanograd_a_val.grad) < TOLERANCE
+    assert abs(torch_c_val.grad.item() - nanograd_c_val.grad) < TOLERANCE
 
 
 def test_tanh():
-    na = Val(2.0)
-    nc = na.tanh()
-    nc.backward()
-    print(na.grad)
+    """Test tanh"""
+    nanograd_a_val = Val(2.0)
+    nanograd_c_val = nanograd_a_val.tanh()
+    nanograd_c_val.backward()
+    print(nanograd_a_val.grad)
 
-    at = torch.tensor(2.0, requires_grad=True)
-    ct = at.tanh()
-    ct.retain_grad()
-    ct.backward()
-    assert abs(nc.item() - ct.item()) < TOLERANCE
-    assert abs(at.grad.item() - na.grad) < TOLERANCE
-    assert abs(ct.grad.item() - nc.grad) < TOLERANCE
+    torch_a_val = torch.tensor(2.0, requires_grad=True)
+    torch_c_val = torch_a_val.tanh()
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
+    assert abs(nanograd_c_val.item() - torch_c_val.item()) < TOLERANCE
+    assert abs(torch_a_val.grad.item() - nanograd_a_val.grad) < TOLERANCE
+    assert abs(torch_c_val.grad.item() - nanograd_c_val.grad) < TOLERANCE
 
 
 def test_sigmoid():
-    na = Val(2.0)
-    nc = na.sigmoid()
-    nc.backward()
-    print(na.grad)
+    """Test sigmoid"""
+    nanograd_a_val = Val(2.0)
+    nanograd_c_val = nanograd_a_val.sigmoid()
+    nanograd_c_val.backward()
+    print(nanograd_a_val.grad)
 
-    at = torch.tensor(2.0, requires_grad=True)
-    ct = at.sigmoid()
-    ct.retain_grad()
-    ct.backward()
-    assert abs(nc.item() - ct.item()) < TOLERANCE
-    assert abs(at.grad.item() - na.grad) < TOLERANCE
-    assert abs(ct.grad.item() - nc.grad) < TOLERANCE
-
-
-"""Micrograds Test"""
+    torch_a_val = torch.tensor(2.0, requires_grad=True)
+    torch_c_val = torch_a_val.sigmoid()
+    torch_c_val.retain_grad()
+    torch_c_val.backward()
+    assert abs(nanograd_c_val.item() - torch_c_val.item()) < TOLERANCE
+    assert abs(torch_a_val.grad.item() - nanograd_a_val.grad) < TOLERANCE
+    assert abs(torch_c_val.grad.item() - nanograd_c_val.grad) < TOLERANCE
 
 
-def test_sanity_check_relu():
-    x = Val(-4.0)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
+# Micrograds Test
+def test_relu():
+    """Test relu"""
+    x_var = Val(-4.0)
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).relu()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_grad_var, y_grad_var = x_var, y_var
 
-    x = torch.Tensor([-4.0]).double()
-    x.requires_grad = True
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
-    assert ymg.data == ypt.data.item()
-    assert xmg.grad == xpt.grad.item()
-
-
-def test_sanity_check_tanh():
-    x = Val(-4.0)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).tanh()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
-
-    x = torch.Tensor([-4.0]).double()
-    x.requires_grad = True
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).tanh()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
-    assert ymg.data == ypt.data.item()
-    assert xmg.grad == xpt.grad.item()
+    x_var = torch.Tensor([-4.0]).double()
+    x_var.requires_grad = True
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).relu()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_torch_var, y_torch_var = x_var, y_var
+    assert y_grad_var.data == y_torch_var.data.item()
+    assert x_grad_var.grad == x_torch_var.grad.item()
 
 
-def test_sanity_check_sigmoid():
-    x = Val(-4.0)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).sigmoid()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
+def test_tanh_2():
+    """Test tanh additional"""
+    x_var = Val(-4.0)
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).tanh()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_nano_grad, y_nano_grad = x_var, y_var
 
-    x = torch.Tensor([-4.0]).double()
-    x.requires_grad = True
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).sigmoid()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
-    assert ymg.data == ypt.data.item()
-    assert xmg.grad == xpt.grad.item()
+    x_var = torch.Tensor([-4.0]).double()
+    x_var.requires_grad = True
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).tanh()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_torch_grad, y_torch_grad = x_var, y_var
+    assert y_nano_grad.data == y_torch_grad.data.item()
+    assert x_nano_grad.grad == x_torch_grad.grad.item()
+
+
+def test_sigmoid_2():
+    """Test sigmoid additional"""
+    x_var = Val(-4.0)
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).sigmoid()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_nano_grad, y_nano_grad = x_var, y_var
+
+    x_var = torch.Tensor([-4.0]).double()
+    x_var.requires_grad = True
+    z_var = 2 * x_var + 2 + x_var
+    q_var = z_var.relu() + z_var * x_var
+    h_var = (z_var * z_var).sigmoid()
+    y_var = h_var + q_var + q_var * x_var
+    y_var.backward()
+    x_torch_grad, y_torch_grad = x_var, y_var
+    assert y_nano_grad.data == y_torch_grad.data.item()
+    assert x_nano_grad.grad == x_torch_grad.grad.item()
 
 
 def test_more_ops():
-    a = Val(-4.0)
-    b = Val(2.0)
-    c = a + b
-    d = a * b + b**3
-    c += c + 1
-    c += 1 + c + (-a)
-    d += d * 2 + (b + a).relu()
-    d += 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g += 10.0 / f
-    g.backward()
-    amg, bmg, gmg = a, b, g
+    """Test extended operations"""
+    a_var = Val(-4.0)
+    b_var = Val(2.0)
+    c_var = a_var + b_var
+    d_var = a_var * b_var + b_var**3
+    c_var += c_var + 1
+    c_var += 1 + c_var + (-a_var)
+    d_var += d_var * 2 + (b_var + a_var).relu()
+    d_var += 3 * d_var + (b_var - a_var).relu()
+    e_var = c_var - d_var
+    f_var = e_var**2
+    g_var = f_var / 2.0
+    g_var += 10.0 / f_var
+    g_var.backward()
+    a_nano_grad, b_nano_grad, c_nano_grad = a_var, b_var, g_var
 
-    a = torch.Tensor([-4.0]).double()
-    b = torch.Tensor([2.0]).double()
-    a.requires_grad = True
-    b.requires_grad = True
-    c = a + b
-    d = a * b + b**3
-    c = c + c + 1
-    c = c + 1 + c + (-a)
-    d = d + d * 2 + (b + a).relu()
-    d = d + 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g = g + 10.0 / f
-    g.backward()
-    apt, bpt, gpt = a, b, g
+    a_var = torch.Tensor([-4.0]).double()
+    b_var = torch.Tensor([2.0]).double()
+    a_var.requires_grad = True
+    b_var.requires_grad = True
+    c_var = a_var + b_var
+    d_var = a_var * b_var + b_var**3
+    c_var = c_var + c_var + 1
+    c_var = c_var + 1 + c_var + (-a_var)
+    d_var = d_var + d_var * 2 + (b_var + a_var).relu()
+    d_var = d_var + 3 * d_var + (b_var - a_var).relu()
+    e_var = c_var - d_var
+    f_var = e_var**2
+    g_var = f_var / 2.0
+    g_var = g_var + 10.0 / f_var
+    g_var.backward()
+    a_torch, b_torch, g_torch = a_var, b_var, g_var
 
     tol = 1e-6
-    assert abs(gmg.data - gpt.data.item()) < tol
-    assert abs(amg.grad - apt.grad.item()) < tol
-    assert abs(bmg.grad - bpt.grad.item()) < tol
+    assert abs(c_nano_grad.data - g_torch.data.item()) < tol
+    assert abs(a_nano_grad.grad - a_torch.grad.item()) < tol
+    assert abs(b_nano_grad.grad - b_torch.grad.item()) < tol
